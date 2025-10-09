488d9bb596a2de1cfd9804925efde6723d15b96e
singhisking5000 <jamshedkalsi51@gmail.com>
2025-10-08 21:30:30 -0400

    MiniChange

--- workspace/HelloWorldSwing.java
+++ workspace/HelloWorldSwing.java
@@ -10,15 +10,17 @@ public class HelloWorldSwing {
         //Create and set up the window.
         JFrame frame = new JFrame("HelloWorldSwing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(240,240);
 
         //Add the ubiquitous "Hello World" label.
         JLabel label = new JLabel("Hello World");
         JButton button = new JButton("Press me");
+        //JButton.setSize(10,10);
         frame.getContentPane().add(label);
         frame.getContentPane().add(button);
 
         //Display the window.
-        frame.pack();
+        //frame.pack();
         frame.setVisible(true);
     }
 
