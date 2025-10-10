a13dda6fed7907b9b9425c1fba2185d9a4e5aee2
singhisking5000 <73306374+singhisking5000@users.noreply.github.com>
2025-10-10 16:49:52 +0000

    haha, no :|

--- workspace/HelloWorldSwing.java
+++ workspace/HelloWorldSwing.java
@@ -54,5 +54,11 @@ public class HelloWorldSwing implements ActionListener {
         });
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
    
 }
