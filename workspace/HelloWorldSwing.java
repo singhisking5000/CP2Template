f060c76339da3566224c39dfe1f486628c334cf0
singhisking5000 <73306374+singhisking5000@users.noreply.github.com>
2025-11-06 17:50:41 +0000

    thank  u mr m

--- workspace/HelloWorldSwing.java
+++ workspace/HelloWorldSwing.java
@@ -7,6 +7,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,8 +16,8 @@ public class HelloWorldSwing {
     {
         JFrame frame = new JFrame();
         frame.setTitle("Texas Holdem");
-        int frameWidth = 1080;
-        int frameHeight = 840;
+        int frameWidth = 1280;
+        int frameHeight = 720;
         frame.setSize(frameWidth, frameHeight);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
@@ -25,13 +26,13 @@ public class HelloWorldSwing {
         gameArea.setLayout(new GridBagLayout());
 
         JPanel communityCards = new JPanel();
-        communityCards.setSize(new Dimension(1080, 420));
-        communityCards.setPreferredSize(new Dimension(1080, 420));
+        communityCards.setSize(new Dimension(frameWidth, frameHeight/2));
+        communityCards.setPreferredSize(new Dimension(frameWidth, frameHeight/2));
         communityCards.setBackground(Color.yellow);
 
         JPanel player1 = new JPanel();
-        player1.setSize(new Dimension(360,420));
-        player1.setPreferredSize(new Dimension(360, 420));
+        player1.setSize(new Dimension(frameWidth/3, frameHeight/2));
+        player1.setPreferredSize(new Dimension(frameWidth/3, frameHeight/2));
         player1.setBackground(Color.ORANGE);
         
 
@@ -39,44 +40,36 @@ public class HelloWorldSwing {
         // -------------------------------------------------------------- \
         // SPECIAL CASE - CURRENTLY NOT FINISHED
         JPanel buttonsAndDeck = new JPanel();
-        buttonsAndDeck.setSize(new Dimension(360, 420));
-        buttonsAndDeck.setPreferredSize(new Dimension(360, 420));
-        buttonsAndDeck.setBorder(BorderFactory.createLineBorder(Color.RED, 3));
+        buttonsAndDeck.setSize(new Dimension(frameWidth/3, frameHeight/2));
+        buttonsAndDeck.setPreferredSize(new Dimension(frameWidth/3, frameHeight/2));
+        //buttonsAndDeck.setBorder(BorderFactory.createLineBorder(Color.RED, 3));
         buttonsAndDeck.setLayout(new GridBagLayout());
-
+        
+        // INSIDE BUTTONSANDDECK
         JPanel buttons = new JPanel();
         buttons.setBackground(Color.BLUE);
-        buttons.setSize(new Dimension(360, 210));
-        buttons.setPreferredSize(new Dimension(360, 210));
-        buttons.setMinimumSize(new Dimension(360, 210));
+        buttons.setSize(new Dimension(frameWidth/3, frameHeight/4));
+        buttons.setPreferredSize(new Dimension(frameWidth/3, frameHeight/4));
+        buttons.setMinimumSize(new Dimension(frameWidth/3, frameHeight/4));
         // buttons.setLayout();
         JPanel drawPile = new JPanel();
         drawPile.setBackground(Color.MAGENTA);
-        drawPile.setSize(new Dimension(180, 210));
-        drawPile.setPreferredSize(new Dimension(180, 210));
+        drawPile.setSize(new Dimension(frameWidth/6, frameHeight/4));
+        drawPile.setPreferredSize(new Dimension(frameWidth/6, frameHeight/4));
+        drawPile.setMinimumSize(new Dimension(frameWidth/6, frameHeight/4));
 
         JPanel discard = new JPanel();
         discard.setBackground(Color.GREEN);
-        discard.setSize(new Dimension(180, 210));
-        discard.setPreferredSize(new Dimension(180, 210));
-
-        GridBagConstraints tempC = new GridBagConstraints();
-        tempC.anchor = GridBagConstraints.NORTHWEST;
-        tempC.fill = GridBagConstraints.BOTH;
-        changeConstraints(tempC, 0, 0, 2, 1);
-        buttonsAndDeck.add(buttons, tempC);
-        changeConstraints(tempC, 0, 1, 1, 1);
-        buttonsAndDeck.add(drawPile);
-        changeConstraints(tempC, 1, 1, 1, 1);
-        buttonsAndDeck.add(discard);
-
+        discard.setSize(new Dimension(frameWidth/6, frameHeight/4));
+        discard.setPreferredSize(new Dimension(frameWidth/6, frameHeight/4));
+        discard.setMinimumSize(new Dimension(frameWidth/6, frameHeight/4));
         // -------------------------------------------------------------- \
 
 
 
         JPanel player2 = new JPanel();
-        player2.setSize(new Dimension(360, 420));
-        player2.setPreferredSize(new Dimension(360, 420));
+        player2.setSize(new Dimension(frameWidth/3, frameHeight/2));
+        player2.setPreferredSize(new Dimension(frameWidth/3, frameHeight/2));
         player2.setBackground(Color.PINK);
 
         GridBagConstraints c = new GridBagConstraints();
@@ -94,6 +87,21 @@ public class HelloWorldSwing {
 
         changeConstraints(c, 2, 1, 1, 1);
         gameArea.add(player2, c);
+
+
+        
+        //
+        GridBagConstraints tempC = new GridBagConstraints();
+        tempC.anchor = GridBagConstraints.PAGE_START;
+        tempC.fill = GridBagConstraints.BOTH;
+
+       // tempC.insets = new Insets(5, 10, 5, 10);
+        changeConstraints(tempC, 0, 0, 2, 1);
+        buttonsAndDeck.add(buttons, tempC);
+        changeConstraints(tempC, 0, 1, 1, 1);
+        buttonsAndDeck.add(drawPile, tempC);
+        changeConstraints(tempC, 1, 1, 1, 1);
+        buttonsAndDeck.add(discard, tempC);
 
         frame.add(gameArea);
         frame.setVisible(true);
